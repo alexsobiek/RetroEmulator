f54a197cce18d6ab8e7a1f1b2f1f65d0fd333f05
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-09-29 11:18:35 -0500

    Added more advanced example

--- src/main/java/com/alexsobiek/retroemulator/examples/HelloWorld.java
+++ src/main/java/com/alexsobiek/retroemulator/examples/HelloWorld.java
@@ -6,7 +6,17 @@ import java.awt.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Emulator emulator = new Emulator();                                     // Call a new instance of the Emulator
-        emulator.printString("HELLO WORLD", 0, 0, Color.BLUE, Color.BLACK);     // Prints HELLO WORLD in blue
+        Emulator emulator = new Emulator();
+
+        emulator.printString("HELLO", 0,0);
+        emulator.printString("WORLD", 0,35);
+
+        emulator.printString("+-------------------+", 10, 10, Color.BLACK, Color.LIGHT_GRAY);
+        emulator.printString("|  RETRO EMULATOR   |", 11, 10, Color.BLACK, Color.LIGHT_GRAY);
+        emulator.printString("| LOYOLA UNIVERSITY |", 12, 10, Color.BLACK, Color.LIGHT_GRAY);
+        emulator.printString("+-------------------+", 13, 10, Color.BLACK, Color.LIGHT_GRAY);
+
+        emulator.printString("HELLO", 24,0);
+        emulator.printString("WORLD", 24,35);
     }
 }
